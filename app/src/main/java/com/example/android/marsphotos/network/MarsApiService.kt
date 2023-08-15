d3f6383730ec28f0fc94b2bbb374a71ca16ca144
Tabuletto <robomez@mail.ru>
2023-08-15 17:27:28 +0300

    Прикручен Retrofit, получает данные с сервера, отображает строкой

---
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -0,0 +1,24 @@
+package com.example.android.marsphotos.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL =
+	"https://android-kotlin-fun-mars-server.appspot.com"
+
+private val retrofit = Retrofit.Builder()
+	.baseUrl(BASE_URL)
+	.addConverterFactory(ScalarsConverterFactory.create())
+	.build()
+
+interface MarsApiService {
+	@GET("photos")
+	suspend fun getPhotos() : String
+}
+
+object MarsApi {
+	val retrofitService : MarsApiService by lazy {
+		retrofit.create(MarsApiService::class.java)
+	}
+}
